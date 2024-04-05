e3b56ec93dd8839d3681049c23df56b19b1108d6
Amanda Ike <amonte13@my.centennialcollege.ca>
2024-04-04 20:29:15 -0400

    Consumer artifact feed

---
+++ ConsumeArtifact/Program.cs
@@ -0,0 +1,16 @@
+﻿using UtilityLibraries;
+
+namespace ConsumeArtifact
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test Number 1 - Input: hello world");
+            Console.WriteLine(StringLibrary.StartsWithUpper("hello world"));
+
+            Console.WriteLine("Test Number 2 - Input: Good afternoon");
+            Console.WriteLine(StringLibrary.StartsWithUpper("Good afternoon"));
+        }
+    }
+}
